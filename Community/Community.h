64fb15755492136f25ae522ae3caf4768fe4100b
Dan Esrey <danesrey@dans-macbook-air.local>
2016-10-04 18:46:06 -0400

    Initial Commit

---
+++ Community/Community.h
@@ -0,0 +1,19 @@
+//
+//  Community.h
+//  Community
+//
+//  Created by Dan Esrey on 2016/04/10.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Community.
+FOUNDATION_EXPORT double CommunityVersionNumber;
+
+//! Project version string for Community.
+FOUNDATION_EXPORT const unsigned char CommunityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Community/PublicHeader.h>
+
+
